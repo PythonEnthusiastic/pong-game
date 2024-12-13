82f275887671c2e20a3a7256431fdcca9a95d311
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-12-12 19:12:26 -0700

    Added rackets, user able to move rackets, set up racket boundaries

--- src/main.py
+++ src/main.py
@@ -1,23 +1,71 @@
 import pygame
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
+pygame.init()
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        screen.fill("black")
+class Racket:
+    def __init__(self, pos_x):
+        self.racket = pygame.Rect(pos_x, 30, 15, 110)
+        self.racket_speed = 7
 
-        pygame.display.flip()
+    def move(self, direction):
+        if direction == "up":
+            self.racket.y -= self.racket_speed
+        elif direction == "down":
+            self.racket.y += self.racket_speed
 
-        clock.tick(60)
+        self.boundary()
+
+    def boundary(self):
+        y_bound = (0, 720 - 110)
+
+        if self.racket.y > y_bound[1]:
+            self.racket.y = y_bound[1]
+        elif self.racket.y < y_bound[0]:
+            self.racket.y = y_bound[0]
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, "white", self.racket)
+
+class Pong:
+    def __init__(self):
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.first_racket = Racket(20)
+        self.second_racket = Racket((1280 - 15) - 23)
+
+    def handle_player_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.first_racket.move("up")
+        if keys[pygame.K_s]:
+            self.first_racket.move("down")
+        if keys[pygame.K_UP]:
+            self.second_racket.move("up")
+        if keys[pygame.K_DOWN]:
+            self.second_racket.move("down")
+
+    def update_racket_position(self):
+        self.first_racket.draw(self.screen)
+        self.second_racket.draw(self.screen)
+            
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill("black")
+
+            self.handle_player_keys()
+            self.update_racket_position()
+
+            pygame.display.flip()
+
+            self.clock.tick(60)
     
-    pygame.quit()
+        pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    game = Pong()
+    game.run()
