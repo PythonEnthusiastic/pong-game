71826c6ee1b6a02d47520ba83fe601934fc33e17
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-12-12 20:54:53 -0700

    Added bouncing when touch one of the reckets (only bakc and forth on the x axis)

--- src/main.py
+++ src/main.py
@@ -6,41 +6,50 @@ SCREEN_SIZE = (1280, 720)
 
 class Racket:
     def __init__(self, pos_x):
-        self.racket = pygame.Rect(pos_x, 30, 15, 110)
+        self.pos_x = pos_x
+        self.pos_y = (SCREEN_SIZE[1] / 2) - 50
         self.racket_speed = 7
 
     def move(self, direction):
         if direction == "up":
-            self.racket.y -= self.racket_speed
+            self.pos_y -= self.racket_speed
         elif direction == "down":
-            self.racket.y += self.racket_speed
+            self.pos_y += self.racket_speed
 
         self.boundary()
 
     def boundary(self):
         y_bound = (0, 720 - 110)
 
-        if self.racket.y > y_bound[1]:
-            self.racket.y = y_bound[1]
-        elif self.racket.y < y_bound[0]:
-            self.racket.y = y_bound[0]
+        if self.pos_y > y_bound[1]:
+            self.pos_y = y_bound[1]
+        elif self.pos_y < y_bound[0]:
+            self.pos_y = y_bound[0]
 
     def draw(self, screen):
-        pygame.draw.rect(screen, "white", self.racket)
+        pygame.draw.rect(screen, "white", pygame.Rect(self.pos_x, self.pos_y, 20, 110))
 
 class Ball:
     def __init__(self):
-        self.radius = 15
+        self.radius = 20
         self.pos_x = SCREEN_SIZE[0]/2
         self.pos_y = SCREEN_SIZE[1]/2
-        self.ball_speed = 10
+        self.speed_x = 10
+        self.speed_y = 0
 
     def move(self):
-        self.pos_x += self.ball_speed
+        self.pos_x += self.speed_x
+        self.pos_y += self.speed_y
         self.boundary()
 
-    def collision(self):
-        pass
+    def collision(self, racket1, racket2):
+        racket1_pos = [racket1.pos_x, racket1.pos_y]
+        racket2_pos = [racket2.pos_x, racket2.pos_y]
+
+        if ((racket1_pos[0] - 40) <= self.pos_x <= (racket1_pos[0] + 40)) and ((racket1_pos[1] - 110 - 20) <= self.pos_y <= racket1_pos[1] + 110 + 20):
+            self.speed_x *= -1
+        elif ((racket2_pos[0] - 40) <= self.pos_x <= (racket2_pos[0] + 40)) and ((racket2_pos[1] - 110 - 20) <= self.pos_y <= racket2_pos[1] + 110 + 20):
+            self.speed_x *= -1
 
     def boundary(self):
         if self.pos_x > SCREEN_SIZE[0] or self.pos_x < 0:
@@ -50,7 +59,6 @@ class Ball:
     def draw(self, screen):
         pygame.draw.circle(screen, "red", (self.pos_x, self.pos_y), self.radius)
 
-
 class Pong:
     def __init__(self):
         self.running = True
@@ -59,6 +67,8 @@ class Pong:
         self.first_racket = Racket(20)
         self.second_racket = Racket((1280 - 15) - 23)
         self.ball = Ball()
+        self.second_scoreboard = 0
+        self.first_scoreboard = 0
 
     def handle_player_keys(self):
         keys = pygame.key.get_pressed()
@@ -71,7 +81,11 @@ class Pong:
         if keys[pygame.K_DOWN]:
             self.second_racket.move("down")
 
-    def update_positions(self):
+    def handle_ball(self):
+        self.ball.move()
+        self.ball.collision(self.first_racket, self.second_racket)
+
+    def draw_objects(self):
         self.first_racket.draw(self.screen)
         self.second_racket.draw(self.screen)
         self.ball.draw(self.screen)
@@ -81,12 +95,12 @@ class Pong:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
             self.screen.fill("black")
 
+
             self.handle_player_keys()
-            self.update_positions()
-            self.ball.move()
+            self.handle_ball()
+            self.draw_objects()
 
             pygame.display.flip()
 
@@ -95,5 +109,5 @@ class Pong:
         pygame.quit()
 
 if __name__ == "__main__":
-    game = Pong()
-    game.run()
+    pong = Pong()
+    pong.run()
