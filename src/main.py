c5ad506f48eb32d4a11996432e3623d182ac5740
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2024-12-12 23:35:25 -0700

    Added check winner, and now game only starts when one of the racket moves

--- src/main.py
+++ src/main.py
@@ -18,6 +18,9 @@ class Racket:
             self.racket.y += self.racket_speed
 
         self.boundary()
+    
+    def racket_reset(self):
+        self.racket.y = (SCREEN_SIZE[1] / 2) - 50
 
     def boundary(self):
         y_bound = (0, 720 - 110)
@@ -33,27 +36,36 @@ class Racket:
 class Ball:
     def __init__(self):
         self.ball = pygame.Rect(SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/2, BALL_RADIUS, BALL_RADIUS)
-        self.speed_x = 10
+        self.speed_x = 6
         self.speed_y = 0
 
     def move(self):
         self.ball.x += self.speed_x
         self.ball.y += self.speed_y
-        self.boundary()
 
     def collision(self, racket1, racket2):
         if self.ball.colliderect(racket1.racket) or self.ball.colliderect(racket2.racket):
-            self.speed_x *= -1
             if self.speed_y == 0:
+                self.speed_x = 10
                 self.speed_y = 5
+            self.speed_x *= -1
 
         if self.ball.y <= 0 or self.ball.y >= SCREEN_SIZE[1]:
             self.speed_y *= -1
 
+    def ball_reset(self):
+        self.speed_x = 6
+        self.ball.x = SCREEN_SIZE[0]/2
+        self.ball.y = SCREEN_SIZE[1]/2
+
     def boundary(self):
-        if self.ball.x > SCREEN_SIZE[0] or self.ball.x < 0:
-            self.ball.x = SCREEN_SIZE[0]/2
-            self.ball.y = SCREEN_SIZE[1]/2
+        if self.ball.x > SCREEN_SIZE[0]:
+            self.ball_reset()
+            return -1
+        if self.ball.x < 0:
+            self.ball_reset()
+            return 1
+        return 0
 
     def draw(self, screen):
         pygame.draw.ellipse(screen, "red", self.ball)
@@ -61,6 +73,7 @@ class Ball:
 class Pong:
     def __init__(self):
         self.running = True
+        self.player_start = False
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1280, 720))
         self.first_racket = Racket(20)
@@ -73,18 +86,22 @@ class Pong:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.first_racket.move("up")
+            self.player_start = True
         if keys[pygame.K_s]:
             self.first_racket.move("down")
+            self.player_start = True
         if keys[pygame.K_UP]:
             self.second_racket.move("up")
+            self.player_start = True
         if keys[pygame.K_DOWN]:
             self.second_racket.move("down")
+            self.player_start = True
 
     def display_score(self, num, pos_x):
         font = pygame.font.Font(None, 100)
         text_surface = font.render(str(num), True, (255, 255, 255))
         text_rect = text_surface.get_rect()
-        text_rect.center = (SCREEN_SIZE[0] // 2 + pos_x, SCREEN_SIZE[1] - 30)
+        text_rect.center = (SCREEN_SIZE[0] // 2 + pos_x, 50)
         self.screen.blit(text_surface, text_rect)
 
     def handle_ball(self):
@@ -95,6 +112,24 @@ class Pong:
         self.first_racket.draw(self.screen)
         self.second_racket.draw(self.screen)
         self.ball.draw(self.screen)
+
+    def full_reset(self):
+        self.ball.ball_reset()
+        self.first_racket.racket_reset()
+        self.second_racket.racket_reset()
+        self.first_scoreboard = 0
+        self.second_scoreboard = 0
+        self.player_start = False
+    
+    def check_winner(self):
+        scorer = self.ball.boundary()
+        if scorer == 1:
+            self.first_scoreboard += 1
+        elif scorer == -1:
+            self.second_scoreboard += 1
+
+        if self.first_scoreboard == 10 or self.second_scoreboard == 10:
+            self.full_reset()
             
     def run(self):
         while self.running:
@@ -106,7 +141,9 @@ class Pong:
             self.display_score(self.first_scoreboard, 50)
             self.display_score(self.second_scoreboard, -50)
             self.handle_player_keys()
-            self.handle_ball()
+            if self.player_start:
+                self.handle_ball()
+                self.check_winner()
             self.draw_objects()
 
             pygame.display.flip()
